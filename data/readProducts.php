cd0db47c051d8442560124638c42b50d55882e8a
huda <sadyahmed43@gmail.com>
2021-12-12 00:43:37 +1100

    finishing the txt file task

---
+++ data/readProducts.php
@@ -0,0 +1,17 @@
+<?php 
+ 
+$delimiter = ","; 
+$content   = file('products.txt'); 
+
+echo "<table>\n"; 
+foreach ($content as $row) { 
+	echo "\t<tr>\n"; 
+	$cells = explode($delimiter, $row); 
+	foreach ($cells as $cell) { 
+		echo sprintf("\t\t<td>%s</td>\n", $cell); 
+	} 
+	echo "\t</tr>\n"; 
+}
+echo "</table>\n";
+
+?>
